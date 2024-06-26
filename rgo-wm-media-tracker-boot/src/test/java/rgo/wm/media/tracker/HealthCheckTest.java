f3f0723e6b87595b4e9ba2c361d9acc334ef1716
evgeny-mordyasov <michisig11i7@gmail.com>
2024-06-26 23:02:55 +0700

    create a real server for HealthCheckTest

--- rgo-wm-media-tracker-boot/src/test/java/rgo/wm/media/tracker/HealthCheckTest.java
+++ rgo-wm-media-tracker-boot/src/test/java/rgo/wm/media/tracker/HealthCheckTest.java
@@ -1,51 +1,54 @@
 package rgo.wm.media.tracker;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.health.Status;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.client.RestTemplate;
 import rgo.wm.media.tracker.tests.Containers;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
 @ActiveProfiles("test")
-@AutoConfigureMockMvc
 @Import(Containers.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class HealthCheckTest {
 
-    @Autowired
-    private MockMvc mvc;
+    private static final String BASE_URL = "http://localhost:";
+    private static final RestTemplate REST = new RestTemplate();
+
+    @LocalServerPort
+    private int port;
 
     @Test
-    void health_up() throws Exception {
+    void health_up() {
         checkUp("/actuator/health");
     }
 
     @Test
-    void livenessProbe_up() throws Exception {
+    void livenessProbe_up() {
         checkUp("/actuator/health/liveness");
     }
 
     @Test
-    void readinessProbe_up() throws Exception {
+    void readinessProbe_up() {
         checkUp("/actuator/health/readiness");
     }
 
     @Test
-    void db_up() throws Exception {
+    void db_up() {
         checkUp("/actuator/health/db");
     }
 
-    private void checkUp(String url) throws Exception {
-        mvc.perform(get(url))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.status").value(Status.UP.getCode()));
+    private void checkUp(String url) {
+        StatusV2 status = REST.getForObject(BASE_URL + port + url, StatusV2.class);
+        assertThat(status).isNotNull();
+        assertThat(status.status()).isEqualTo(Status.UP.getCode());
+    }
+
+    private record StatusV2(String status) {
     }
 }
+
