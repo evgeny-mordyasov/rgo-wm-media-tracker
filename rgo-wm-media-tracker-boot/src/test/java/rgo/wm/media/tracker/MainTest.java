5e043c1cf0c2a43d1fa876d4ec5aa0bd6a48bd9d
evgeny-mordyasov <michisig11i7@gmail.com>
2024-08-27 23:14:05 +0700

    silent log for tests and talking log to run locally

--- rgo-wm-media-tracker-boot/src/test/java/rgo/wm/media/tracker/MainTest.java
+++ rgo-wm-media-tracker-boot/src/test/java/rgo/wm/media/tracker/MainTest.java
@@ -6,7 +6,7 @@ import rgo.wm.media.tracker.it.Containers;
 class MainTest {
 
     public static void main(String[] args) {
-        System.setProperty("spring.profiles.active", "test");
+        System.setProperty("spring.profiles.active", "main-test");
         SpringApplication.from(Main::main)
                 .with(Containers.class)
                 .run(args);
